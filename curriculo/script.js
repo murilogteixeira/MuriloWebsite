685839f224c42a774f50cd13b775ff38dceb7808
Murilo Teixeira <murilogteixeira@gmail.com>
2020-08-05 16:33:14 -0300

    Finalizar envio de mensagem da sessao contato

--- curriculo/script.js
+++ curriculo/script.js
@@ -80,3 +80,77 @@ function activateLightMode() {
     document.getElementById('navbar').classList.remove('navbar-dark');
     document.getElementById('navbar').classList.add('navbar-light');
 }
+
+$('#form-contato').submit( () => {
+    const method = 'POST';
+    const url = 'https://api-murilo.mybluemix.net/send-email';
+    const params = new URLSearchParams({
+        nome: $('#inputNome').val(),
+        email: $('#inputEmail').val(),
+        texto: $('#inputText').val()
+    }).toString();
+    const callback = (data) => {
+        console.log(data);
+        if(data.statusOk) {
+            showAlert("success", "Obrigado pelo seu contato!", "Sua mensagem foi enviada e retornarei em breve.");
+        }
+        else {
+            showAlert("danger", "Mensagem não enviada!", "Encontramos um problema ao enviar a mensagem. Tente novamente em alguns instantes e se o problema persistir, por favor, encaminhe diretamente para <a href='mailto:contato@murilot.com' class='alert-link'>contato@murilot.com</a>");
+        }
+    };
+    
+    request(method, url, params, callback);
+    return false;
+})
+
+function showAlert(tipo, titulo, msg) {
+    var alertDiv = document.getElementById('alert');
+
+    var alertContent = `
+        <div class="alert alert-${tipo} alert-dismissible fade show" role="alert">
+            <strong>${titulo}</strong><br>${msg}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    `
+
+    alertDiv.innerHTML = alertContent;
+
+    if(tipo !== 'danger') {
+        $(".alert").delay(5000).slideUp(200, function() {
+            $(this).alert('close');
+        });
+    }
+}
+
+function request(method, url, params, callback) {
+    var ajax = new XMLHttpRequest();
+
+    if(method === 'POST' || method === 'post') {
+        ajax.open(method, url, true);
+        ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        ajax.send(params);
+    }
+    else if (method === 'GET' || method === 'get') {
+        ajax.open(method, url + '/?' + params, true);
+        ajax.send();
+    }
+
+    // Cria um evento para receber o retorno.
+    ajax.onreadystatechange = () => {
+    // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            // Retorno do Ajax
+            var ajaxReturn = ajax.responseText;
+            var data = JSON.parse(ajaxReturn);
+            callback(data);
+        }
+        else if (ajax.readyState == 4 && ajax.status != 200) {
+            callback({
+                'statusOk': false,
+                'msg': 'Mensagem não envidada'
+            });
+        }
+    }
+}

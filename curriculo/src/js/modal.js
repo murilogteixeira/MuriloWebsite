b2b4a77825df74c5fc4228b3ade79e9af034a503
Murilo Teixeira <murilogteixeira@gmail.com>
2020-08-08 01:24:04 -0300

    Adicionarr imagens

---
+++ curriculo/src/js/modal.js
@@ -0,0 +1,16 @@
+$('#modalGaleria').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var i = button.data('whatever') // Extract info from data-* attributes
+
+    var modal = $(this)
+    modal.find('#appName').text(projetos[i].nome)
+    modal.find('#appDate').text(projetos[i].data)
+    modal.find('#appDescription').text(projetos[i].descricao)
+    // modal.find('.modal-body input').val(projetos[i].nome)
+
+    modal.find('#appImg').attr('src', projetos[i].linkImg);
+
+    modal.find('#projectLink').attr('href', projetos[i].linkDownload);
+    modal.find('#download-text').text(projetos[i].downloadOn);
+    modal.find('#download-icon').html(projetos[i].downloadOn === 'AppStore' ? '<i class="fab fa-app-store"></i>' : '<i class="fab fa-github"></i>');
+})
